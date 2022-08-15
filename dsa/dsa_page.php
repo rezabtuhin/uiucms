39b85799920d2cd654abcbe17a09aa991d8427ef
shahariarrifat9 <shahriarrifat8@gmail.com>
2022-08-16 02:24:16 +0600

    Updated dsa page with db

--- dsa/dsa_page.php
+++ dsa/dsa_page.php
@@ -235,13 +235,7 @@ $num = mysqli_num_rows($connect);
 												<h3><a href="https://gambolthemes.net/workwise-new/company-profile.html#" title="" class="overview-open">Overview</a> <a href="https://gambolthemes.net/workwise-new/company-profile.html#" title="" class="overview-open"><i class="fa fa-pencil"></i></a></h3>
 												<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque tempor
 													aliquam felis, nec condimentum ipsum commodo id. Vivamus sit amet augue
-													nec urna efficitur tincidunt. Vivamus consectetur aliquam lectus commodo
-													viverra. Nunc eu augue nec arcu efficitur faucibus. Aliquam accumsan ac
-													magna convallis bibendum. Quisque laoreet augue eget augue fermentum
-													scelerisque. Vivamus dignissim mollis est dictum blandit. Nam porta
-													auctor neque sed congue. Nullam rutrum eget ex at maximus. Lorem ipsum
-													dolor sit amet, consectetur adipiscing elit. Donec eget vestibulum
-													lorem.</p>
+													</p>
 											</div>
 											<div class="user-profile-ov st2">
 												<h3><a href="https://gambolthemes.net/workwise-new/company-profile.html#" title="" class="esp-bx-open">Establish Since </a><a href="https://gambolthemes.net/workwise-new/company-profile.html#" title="" class="esp-bx-open"><i class="fa fa-pencil"></i></a> <a href="https://gambolthemes.net/workwise-new/company-profile.html#" title="" class="esp-bx-open"><i class="fa fa-plus-square"></i></a>
