771dbc07f44d9e9de89b1ed1262d11a2c59e1346
Afsana Airin <aairin191172@bscse.uiu.ac.bd>
2022-08-16 11:01:32 +0600

    login file raw html

---
+++ login/login.php
@@ -0,0 +1,73 @@
+
+
+
+<!DOCTYPE html>
+<html>
+<head>
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+
+    <title>Login</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="">
+    <meta name="keywords" content="">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" crossorigin="anonymous">
+    <script src="https://kit.fontawesome.com/b80eba6c42.js" crossorigin="anonymous"></script>
+</head>
+<style>
+
+
+</style>
+<body class="sign-in" data-new-gr-c-s-check-loaded="14.1073.0" data-gr-ext-installed="">
+    <div class="wrapper">
+        <div class="sign-in-page">
+            <div class="signin-popup">
+                <div class="signin-pop">
+                    <div class="row">
+                        <div class="col-lg-6">
+                            <div class="cmp-info">
+                                <div class="cm-logo">
+                                    <h3 style="font-size:18px;font-weight:700;"> <strong> UIU Club Management System </strong></h3>
+                                    <hr>
+                                    <p>UiU Club Management System is a platform which represents all the clubs and forums of UIU</p>
+                                </div>
+                                <img src="./login_files/cm-main-img.png" alt="">
+                            </div>
+                        </div>
+                        <div class="col-lg-6">
+                            <div class="login-sec">
+                                
+                                <div class="sign_in_sec current" id="tab-1">
+                                    <h3>Log In</h3>
+                                    <form action="login_verification.php" method="post">
+                                        <div class="row">
+                                            <div class="col-lg-12 no-pdd">
+                                                <div class="sn-field">
+                                                    <input type="text" name="username" placeholder="Username">
+                                                    <i class="fa-solid fa-user"></i>
+                                                </div>
+                                            </div>
+                                            <div class="col-lg-12 no-pdd">
+                                                <div class="sn-field">
+                                                    <input type="password" name="password" placeholder="Password">
+                                                    <i class="fa-solid fa-lock"></i>
+                                                </div>
+                                            </div>
+                                            
+                                            <div class="col-lg-12 no-pdd">
+                                                <button type="submit" value="submit" name="submit">Log In</button>
+                                            </div>
+                                        </div>
+                                    </form>
+                                    
+                                </div>
+                                
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            
+        </div>
+    </div>
+</body>
+</html>
