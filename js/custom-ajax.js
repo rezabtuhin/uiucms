11786404f8332d093e0c1c2dfcab7185be24fc3c
Sharita-Rahman <106440232+Sharita-Rahman@users.noreply.github.com>
2022-09-20 11:41:01 +0600

    required js

--- js/custom-ajax.js
+++ js/custom-ajax.js
@@ -25,3 +25,31 @@ const updateStatus= function(id){
     });
 
 }
+
+const updateStatusMem= function(id){
+
+    $.ajax({    
+        type: "POST",
+        url: "./includes/update-status.php",
+        data:{updateId:id},            
+        dataType: "html",                  
+        success: function(data){  
+      
+           if(data==1){
+            $("#statusBtn"+id).text("Disapprove")
+           }else if(data==0){
+            $("#statusBtn"+id).text("Approve")
+           }
+           // change status btn coloe
+           if($("#statusBtn"+id).hasClass("approve")){
+            $("#statusBtn"+id).removeClass("approve");
+            $("#statusBtn"+id).addClass("disapprove");
+           }else if($("#statusBtn"+id).hasClass("disapprove")){
+            $("#statusBtn"+id).removeClass("disapprove");
+            $("#statusBtn"+id).addClass("approve");
+           }
+           
+        }
+    });
+
+}
